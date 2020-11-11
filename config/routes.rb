b121ff3f166b1b300cd0403af423bd56d331d634
Sophia Denney <sophiad14@kabelmail.de>
2020-11-11 14:49:52 +0100

    resto

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get "/restaurants/", to: "restaurants#index"
+  get "/restaurants/new", to: "restaurants#new"
+  post "/restaurants", to: "restaurants#create"
+  get "/restaurants/:id", to: "restaurants#show", as: "restaurant"
+
+  get "/restaurants/:restaurant_id/reviews/new", to: "reviews#new"
+  post "/restaurants/:restaurant_id/reviews", to: "reviews#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
